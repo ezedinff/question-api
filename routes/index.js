a0fa611cedc8bf2038a681dc802d8092dc5a48eb
J@tin <ezedin.fedlu@gmail.com>
2022-03-16 12:52:21 +0300

    add welcome message

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const searchController = require('../controllers/search.controller');
 
+router.get('/', (req, res, next) => {
+    const welcomeMessage = 'Welcome to the search API and you can search for questions by topic. You can use GET /search?q=<topic> to search for questions by topic.';
+    res.status(200).send(welcomeMessage);
+});
 
 /* GET */
 router.get('/search', searchController);
